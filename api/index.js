5c538b07a307e13e92e0aa551a79eeed58bba5da
Sudip Kundu <sudip2003kundu@gmail.com>
2024-07-22 19:30:03 +0530

    allowed cors origin *

--- api/index.js
+++ api/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import { config } from "dotenv";
 import home from "../routes/home.js";
 import gettrain from "../routes/getTrains.js";
-
+import cors from "cors";
 config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use("/", home);
 app.use("/trains", gettrain);
 
